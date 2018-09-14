8ecd11ad79246fe23be6c25a14ea8baf9bf5cc26
Carlos <302642469@qq.com>
2018-09-14 08:27:49 +0800

    加了些改名字函数
    -加了些改名字的函数
    -加了些注释

--- Huffman.h
+++ Huffman.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 #include "define.h"
 #include"statistic.h"
 typedef struct {
@@ -9,16 +9,22 @@ typedef struct {
 
 typedef char **HuffmanCode;
 
+//哈夫曼算法的一部分
 void select2(huffmanTree &HT, int max, int &s);
 
+//Huffman算法的一部分
 void select(huffmanTree &HT, int max, int &s);
 
+//创建哈夫曼树+哈夫曼编码
 void HuffmanTree_Build_and_Code(huffmanTree &HT, HuffmanCode &HC, int *w, int n);
 
+//输出哈夫曼编码
 void huffmanPrint(HuffmanCode code, int len);
 
+//输出哈夫曼编码	带字符
 void huffmanPrint(pWORD words, int sumWord);
 
+//
 void Huffman_Test(pWORD words, int sumWords, HuffmanCode &code);
 
 //sumWord是种类，返回生成文件的字符总数
@@ -31,8 +37,11 @@ void HuffmanTree_Build(huffmanTree &HT, unsigned char* w, int n);
 //解压时候创建的Huffman树
 void buildEncodeHuffmanTree(huffmanTree &tree, unsigned int sumWords, unsigned int node_num);
 
-//改名字
-void changeFileNameToHuf(char *filename);
+//改名字 从TXT到HUF
+void changeFileNameToHuf(char * txt, char * huf);
 
-
+//改名字 从HUF到TXT
 void changeFileNameToTxt(char * huf, char *txt);
+
+//改名字 从旧TXT到压缩后生成的TXT
+void chanFileNameToTxt_new(char *oldTxt, char *newTxt);
